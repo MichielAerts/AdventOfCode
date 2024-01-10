6817a75c44a74a2094007a760456ef29e94cdf48
MichielAerts <michiel.aerts@gmail.com>
2024-01-10 22:12:41 +0100

    day 24 part 2

--- src/main/kotlin/advent2023/day24/Puzzle.kt
+++ src/main/kotlin/advent2023/day24/Puzzle.kt
@@ -2,9 +2,13 @@ package advent2023.day24
 
 import advent2023.utils.mapToPair
 import advent2023.utils.runPuzzle
+import org.apache.commons.math3.linear.Array2DRowRealMatrix
+import org.apache.commons.math3.linear.ArrayRealVector
+import org.apache.commons.math3.linear.LUDecomposition
 import org.paukov.combinatorics3.Generator
 import java.io.File
 import kotlin.math.absoluteValue
+import kotlin.math.roundToLong
 import kotlin.math.sign
 
 const val day = 24
@@ -19,8 +23,35 @@ class Puzzle(private val input: List<String>) {
     }
 
     fun runPart2() {
-        val hailstones = input.map { Hailstone.createHailstone(it) }
-        val range = 7L..27L
+        val (a, b, c) = input.map { Hailstone.createHailstone(it) }
+        // lots of algebra, leading to system of linear equations with 6 unknowns (p0 + v of rock)
+
+        // hailstones a & b and rock r
+        // (vyb - vya) xr + (vxa - vxb) yr + 0 * zr + (ya - yb) vxr + (xb - xa) vyr + 0 vzr
+        val coefficients = arrayOf(
+            longArrayOf(b.v.vy - a.v.vy, a.v.vx - b.v.vx, 0, a.p0.py - b.p0.py, b.p0.px - a.p0.px, 0).map { it.toDouble() }.toDoubleArray(),
+            longArrayOf(b.v.vz - a.v.vz, 0, a.v.vx - b.v.vx, a.p0.pz - b.p0.pz, 0, b.p0.px - a.p0.px).map { it.toDouble() }.toDoubleArray(),
+            longArrayOf(0, b.v.vz - a.v.vz, a.v.vy - b.v.vy, 0, a.p0.pz - b.p0.pz, b.p0.py - a.p0.py).map { it.toDouble() }.toDoubleArray(),
+            longArrayOf(c.v.vy - a.v.vy, a.v.vx - c.v.vx, 0, a.p0.py - c.p0.py, c.p0.px - a.p0.px, 0).map { it.toDouble() }.toDoubleArray(),
+            longArrayOf(c.v.vz - a.v.vz, 0, a.v.vx - c.v.vx, a.p0.pz - c.p0.pz, 0, c.p0.px - a.p0.px).map { it.toDouble() }.toDoubleArray(),
+            longArrayOf(0, c.v.vz - a.v.vz, a.v.vy - c.v.vy, 0, a.p0.pz - c.p0.pz, c.p0.py - a.p0.py).map { it.toDouble() }.toDoubleArray()
+        )
+        val matrix = Array2DRowRealMatrix(coefficients, false)
+        val solver = LUDecomposition(matrix).solver
+
+        // right side 
+        // xb vyb - yb vxb + ya vxa - xa vya 
+        val rhs = doubleArrayOf(
+            (b.p0.px * b.v.vy - b.p0.py * b.v.vx + a.p0.py * a.v.vx - a.p0.px * a.v.vy).toDouble(),
+            (b.p0.px * b.v.vz - b.p0.pz * b.v.vx + a.p0.pz * a.v.vx - a.p0.px * a.v.vz).toDouble(),
+            (b.p0.py * b.v.vz - b.p0.pz * b.v.vy + a.p0.pz * a.v.vy - a.p0.py * a.v.vz).toDouble(),
+            (c.p0.px * c.v.vy - c.p0.py * c.v.vx + a.p0.py * a.v.vx - a.p0.px * a.v.vy).toDouble(),
+            (c.p0.px * c.v.vz - c.p0.pz * c.v.vx + a.p0.pz * a.v.vx - a.p0.px * a.v.vz).toDouble(),
+            (c.p0.py * c.v.vz - c.p0.pz * c.v.vy + a.p0.pz * a.v.vy - a.p0.py * a.v.vz).toDouble(),
+        )
+        val constants = ArrayRealVector(rhs, false)
+        val solution = solver.solve(constants)
+        println(solution.toArray().take(3).sum().roundToLong())
     }
 }
 
