8ae3597c7ac147364eaa1ce50637791b072a8039
MichielAerts <michiel.aerts@gmail.com>
2024-12-01 17:36:03 +0100

    day 1

--- src/main/kotlin/advent2024/day1/Puzzle.kt
+++ src/main/kotlin/advent2024/day1/Puzzle.kt
@@ -1,18 +1,28 @@
 package advent2024.day1
 
 import lib.runPuzzle
+import lib.transpose
 import java.io.File
+import kotlin.math.absoluteValue
 
 const val day = 1
 val file = File("src/main/resources/advent2024/day${day}/input")
 
 class Puzzle(private val input: List<String>) {
     fun runPart1() {
-        println(input)
+        //3   4
+        val (left, right) = input.map { it.split(Regex("\\s+")).map { it.toInt() } }
+            .transpose()
+            .map { it.sorted() }
+        
+        println(left.zip(right).sumOf { (it.first - it.second).absoluteValue })
     }
 
     fun runPart2() {
-        println(input)
+        val (left, right) = input.map { it.split(Regex("\\s+")).map { it.toInt() } }
+            .transpose()
+            .map { it.groupingBy { it }.eachCount() }
+        println(left.entries.sumOf { it.key * it.value * (right[it.key] ?: 0) })
     }
 }
 
